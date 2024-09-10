2725461a96ce7e154e0db1ff67dfa9e091d6a47c
Niko Niittyranta <niko.niittyranta@duodecim.fi>
2024-09-10 13:57:31 +0300

    first commit

---
+++ public/js/frontendConfig.js
@@ -0,0 +1,5 @@
+angular.module("app.config", [])
+.constant("api_prefix", "/")
+.constant("logout_page", "")
+.constant("userinfo_page", "")
+.constant("oda_preview_url", "https://oda1-testi.omaolo-beta.fi/palvelut/oirearviot/");
